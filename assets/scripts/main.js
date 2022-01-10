01d14830fab0acd103ccf0cd8d8dc6e0cf137ae4
HollosT <hollos.tamas@ravensoft.hu>
2022-01-10 10:30:49 +0100

    hamburger menu

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,18 +11,23 @@ const strActive = 'opened';
 // Add class "opened" to <header> when #menu is clicked
 menuElem.addEventListener('click', function (event) {
     event.preventDefault(); // stops the element from doing its usual stuff
+    const icon = menuElem.getElementsByTagName('i')[0];
     if (headerElem.className.indexOf('opened') === -1) {
 
         // if the header element HAS the class 'opened' this code will be read
 
         headerElem.classList.add(strActive); // add the content of strActive to the class attribute on <header>
-        menuElem.innerHTML = 'X'; // change the text inside the button
+
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
 
     } else {
 
         // if the header element DOESN'T have the class 'opened' this code will be read
         headerElem.classList.remove(strActive); // add the content of strActive to the class attribute on <header>
-        menuElem.innerHTML = 'fas'; // change the text inside the button
+
+        icon.classList.add('fa-bars');
+        icon.classList.remove('fa-times');
 
     }
 
